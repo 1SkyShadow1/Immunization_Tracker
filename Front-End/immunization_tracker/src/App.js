aed3158a7af9a90bbc0b8f47b7997c8c5a2780cf
1SkyShadow1 <Eazymoja5@gmail.com>
2024-03-20 22:54:29 +0200

    Addition and updating of some of the frontend code

--- Front-End/immunization_tracker/src/App.js
+++ Front-End/immunization_tracker/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Immunizations from "./Immunizations";
+import AddImmunization from "./AddImmunization";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/immunizations" element={<Immunizations />} />
+          <Route path="/add-immunization" element={<AddImmunization />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
